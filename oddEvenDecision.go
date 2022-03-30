6963c9c14957601feed68e7665152c79800b42ae
jpschew <jps.chew@gmail.com>
2022-03-30 13:26:31 +0800

    added code to check user input is even or odd

--- oddEvenDecision.go
+++ oddEvenDecision.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert code here
+	var number int
+	for {
+		fmt.Println("Please enter a number: ")
+		fmt.Scanln(&number)
+		if number%2 == 0 {
+			fmt.Printf("%d is even.\n", number)
+		} else {
+			fmt.Printf("%d is odd.\n", number)
+		}
+	}
 }
